e70b878672158ef9e6d5c6d0cbb00b0de6064252
Daniel Kojis <dankojis@gmail.com>
2019-07-31 12:05:56 -0700

    update

--- simData_methods.R
+++ simData_methods.R
@@ -7,6 +7,50 @@ f_sim = function (X_data){
   return (y)
 }
 
+
+
+
+
+# CS time structured on y and grouped features
+# method found: https://github.com/Yuancheng-Xu/UCLA-CSST/blob/master/FF_timeSeries/SimData.r
+sim_2 = function(n,T,cor_feature=0.8,var_noise=1,cor_noise=0.8){
+  p = 400
+  p0 = 100
+  data = matrix(0,nrow = n*T, ncol = p+1)
+  
+  #### covariance matrix between features: it is either 0 (independent) or cor_feature ####
+  cov_feature = matrix(0,nrow = p, ncol = p)
+  # cov within the first three modules
+  cov_star = matrix(cor_feature,nrow = p0,ncol = p0)
+  diag(cov_star)=1
+  # put cov_star into cov_feature
+  cov_feature[1:p0,1:p0] = cov_star
+  cov_feature[(p0+1):(2*p0),(p0+1):(2*p0)] = cov_star
+  cov_feature[(2*p0+1):(3*p0),(2*p0+1):(3*p0)] = cov_star
+  cov_feature[(3*p0+1):(4*p0),(3*p0+1):(4*p0)] = diag(p0)
+  ####
+  
+  #### covariance matrix of noise #####
+  # the covariance matrix of noise within a unit
+  cov_noise_star = matrix(cor_noise,nrow = T, ncol = T)
+  diag(cov_noise_star) = var_noise
+  # the overal cov matrix has diagonal block matrix as cov_noise_star
+  cov_noise = matrix(0,nrow = n*T, ncol = n*T)
+  for (i in 1:n){
+    cov_noise[(1+(i-1)*T):(i*T),(1+(i-1)*T):(i*T)] = cov_noise_star
+  }
+  ###
+  
+  # Create X matrix
+  data[1:(n*T),1:p] = mvrnorm(n=n*T,rep(0,p),cov_feature)
+  
+  # create label y
+  data[1:(n*T),p+1] = (f_sim(data[1:(n*T),1:p]) 
+                       + mvrnorm(n=1,rep(0,n*T),cov_noise))
+  
+  return(data)
+  
+}
 # Following pdf file: simulation_AR
 # x(i+1) = alpha*x(i) + (1-alpha^2)^0.5*std_normal; in the pdf, alpha = 0.8
 sim_3_RE = function(n,T,cor_feature=0.8,var_noise=1,alpha=0.8){
