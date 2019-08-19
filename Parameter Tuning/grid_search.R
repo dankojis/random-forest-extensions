8490d35cbfbe99eb4bc3c9f0489927009219349d
Daniel Kojis <dankojis@gmail.com>
2019-08-19 13:59:27 -0700

    update

--- Parameter Tuning/grid_search.R
+++ Parameter Tuning/grid_search.R
@@ -23,12 +23,12 @@ fuzzy <- c(TRUE,FALSE)
 
 
 df <- data.frame(n = c(NA),alpha = c(NA), maxdepth_factor_screen= c(NA),
-                       maxdepth_factor_select=c(NA),
-                       MSE = c(NA))   # create empty data frame 
+                 maxdepth_factor_select=c(NA),
+                 MSE = c(NA))   # create empty data frame 
 
 
- 
- 
+
+
 # Formula = as.formula(paste("y~",paste(regress_var,collapse = "+"),
 #                            "|",cluster,"|",
 #                            paste(split_var,collapse = "+")))
@@ -45,47 +45,48 @@ for(n in n_values){
   set.seed(100)
   train <- sim_quad(n,T)
   train$time2 = (train$time)^2
-
+  
   for(a in alpha){
     for(screen in maxdepth_factor_screen){
-       for(select in maxdepth_factor_select){
-              for(f in fuzzy){
-      
-      print(screen)
-      print(select)
-      print(a)
-      print(n)
-      
-      mytree = Longtree(train,fixed_regress=fixed_regress,fixed_split=fixed_split,
-                        var_select=var_select,cluster=cluster,Fuzzy=f, alpha = a,
-                        maxdepth_factor_screen = screen, maxdepth_factor_select = select)
-  
-      mse <- mean((predict(mytree,newdata=test)-test$y)**2)
-      
-      
-      df[i,"n"] <- n
-      df[i, "MSE"] <- mse
-      df[i, "alpha"] <- a
-      df[i, "maxdepth_factor_screen"] <- screen
-      df[i, "maxdepth_factor_select"] <- select
-      df[i, "Fuzzy"] <- f
-      
-      i <- i+1
-        }
+      for(select in maxdepth_factor_select){
+        for(f in fuzzy){
+          
+          print(screen)
+          print(select)
+          print(a)
+          print(n)
+          
+          mytree = Longtree(train,fixed_regress=fixed_regress,fixed_split=fixed_split,
+                            var_select=var_select,cluster=cluster,Fuzzy=f, alpha = a,
+                            maxdepth_factor_screen = screen, maxdepth_factor_select = select)
+          
+          mse <- mean((predict(mytree,newdata=test)-test$y)**2)
+          
+          
+          df[i,"n"] <- n
+          df[i, "MSE"] <- mse
+          df[i, "alpha"] <- a
+          df[i, "maxdepth_factor_screen"] <- screen
+          df[i, "maxdepth_factor_select"] <- select
+          df[i, "Fuzzy"] <- f
+          
+          i <- i+1
         }
       }
     }
-    
-    
-    
-    
   }
+  
+  
+  
+  
+}
 
 
 df$model <- "Longtree"
 
 
-#write.csv(df, "Longtree1200.csv")
+#
+write.csv(df, "Longtree1200.csv")
 
 
 
